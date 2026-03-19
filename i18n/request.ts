f1f9772378579a11f78ef26049c697c55c835b91
GS-Pro2025 <Manager-Support@gspromastermoving.com>
2026-03-19 10:20:39 -0500

    update rute i18n

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,14 +1,15 @@
 import { getRequestConfig } from 'next-intl/server';
-import { notFound } from 'next/navigation';
-import esMessages from '../messages/es.json';
-const locales = ['en', 'es','pt'];
+import { routing } from './routing';
+
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
-  if (!locale || !locales.includes(locale)) {
-    locale = 'en';
+
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
   }
+
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default as typeof esMessages
+    messages: (await import(`../messages/${locale}.json`)).default
   };
 });
